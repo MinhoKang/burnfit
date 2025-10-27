9e396dca4239ce2c4f9434de54dc415703429681
Minho <rkdalsgh0106@naver.com>
2025-10-27 21:59:27 +0900

    react-native-vector-icons를 이용하여 바텁 네비게이션 아이콘 변경 및 스타일 수정

--- src/navigations/BottomTabNavigations.tsx
+++ src/navigations/BottomTabNavigations.tsx
@@ -4,16 +4,56 @@ import { createStaticNavigation } from '@react-navigation/native';
 import CalendarScreen from '../screens/calendar/CalendarScreen';
 import LibraryScreen from '../screens/library/LibraryScreen';
 import MyPageScreen from '../screens/myPage/MyPageScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const BottomTabs = createBottomTabNavigator({
   screens: {
-    HomeScreen: HomeScreen,
-    CalendarScreen: CalendarScreen,
-    LibraryScreen: LibraryScreen,
-    MyPageScreen: MyPageScreen,
+    HomeScreen: {
+      screen: HomeScreen,
+      options: {
+        title: 'HOME',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      },
+    },
+    CalendarScreen: {
+      screen: CalendarScreen,
+      options: {
+        title: 'CALENDAR',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="calendar-outline" color={color} size={size} />
+        ),
+      },
+    },
+    LibraryScreen: {
+      screen: LibraryScreen,
+      options: {
+        title: 'LIBRARY',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="dumbbell" color={color} size={size} />
+        ),
+      },
+    },
+    MyPageScreen: {
+      screen: MyPageScreen,
+      options: {
+        title: 'MY PAGE',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="person-outline" color={color} size={size} />
+        ),
+      },
+    },
   },
   screenOptions: {
     headerShown: false,
+    tabBarActiveTintColor: '#000000',
+    tabBarLabelStyle: {
+      fontWeight: 'bold',
+      fontSize: 12,
+      marginTop: 3,
+    },
   },
 });
 
