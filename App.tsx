7a413b1941d32c5b3c20b04c0b13fe03cda083a7
MinhoKang <rkdalsgh0106@naver.com>
2025-10-29 14:34:44 +0900

    SafeAreaProvider 추가

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
 import { StatusBar, StyleSheet, useColorScheme } from 'react-native';
 import BottomTabNavigations from './src/navigations/BottomTabNavigations';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaView style={styles.container}>
-      <GestureHandlerRootView style={styles.root}>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <BottomTabNavigations />
-      </GestureHandlerRootView>
-    </SafeAreaView>
+    <GestureHandlerRootView style={styles.root}>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.container}>
+          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+          <BottomTabNavigations />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
